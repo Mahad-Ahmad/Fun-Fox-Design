318dace308e9df0963e8e2eb24807190a718d7d7
Mahad Ahmad <mahadahmad921@gmail.com>
2024-03-30 11:25:47 +0000

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,238 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <>
+    <div class="bg-[#FFFFFF] flex flex-col p-[12px_65px_12px_0] w-full box-sizing-border">
+  <img class="absolute top-[0px] right-[-348.9px] w-[1876.6px] h-[834px]" />
+  <img class="absolute top-[389px] right-[-1px] w-[74px] h-[73.4px]" />
+  <img class="absolute top-[648px] right-[-9.2px] w-[82.2px] h-[77.1px]" />
+  <img class="shadow-[0px_0px_11px_0px_rgba(0,0,0,0.25)] rounded-[22.1px] absolute top-[90px] right-[327px] w-[529px] h-[50px]" />
+  <img class="rotate-[-65.733deg] absolute top-[97px] right-[-37.1px] w-[124.1px] h-[122.8px]" />
+  <div class="shadow-[0px_4px_4px_0px_rgba(255,255,255,0.25)] bg-[url('https://fn12.com/api/clusters/cluster-fn12-prod-europe-west1/trialprojects/trial-TQrcZ7Fh49hShjUThC7ojC/assets/images/3a8590a2cec8fa99d0474355d797e1561651c8d4?Expires=1712399866&KeyName=fn12-cdn&Signature=5NpzPMtBwgM27j7_Czv_F6Keub0=')_50%_/_cover_no-repeat] rotate-[-17.103deg] absolute left-[-25px] top-[-23px] w-[179.7px] h-[134px]">
+  </div>
+  <div class="relative m-[0_678px_44px_0] flex flex-row self-center p-[5px_0_6px_6.9px] w-[153px] box-sizing-border">
+    <img class="absolute left-[50%] top-[0px] translate-x-[-50%] w-[153px] h-[41px]" />
+    <img class="absolute left-[50%] top-[1px] translate-x-[-50%] w-[153px] h-[39px]" />
+    <img class="relative m-[0.1px_10.3px_17.9px_0] w-[13.9px] h-[12px]" />
+    <span class="border-[2px_solid_#AA1070] relative break-words font-['Short_Stack'] font-normal text-[24px] text-[#FFFFFF]">
+      Week 1
+    </span>
+  </div>
+  <div class="relative m-[0_25.9px_116px_0] flex flex-row w-[fit-content] box-sizing-border">
+    <img class="m-[24px_27.6px_107.7px_0] w-[61.4px] h-[37.3px]" />
+    <div class="flex flex-col items-center box-sizing-border">
+      <div class="m-[0_0_18px_13.9px] inline-block break-words font-['Short_Stack'] font-normal text-[32px] leading-[1.094] text-[#FFFFFF]">
+        Elements of Story Writing
+      </div>
+      <span class="relative break-words font-['Short_Stack'] font-normal text-[24px] tracking-[-1.4px] leading-[1.208] text-[#1E242F]">
+        Welcome to term 2 of Writers Club. Are you excited for this amazing journey?<br />
+        <br />
+        Over the next week, we will be practicing the different elements of story writing.<br />
+        Our<br />
+        aim is to make stories more interesting and exciting.
+      </span>
+    </div>
+  </div>
+  <div class="relative m-[0_34.8px_8.5px_0] flex flex-row w-[1094.2px] box-sizing-border">
+    <img class="m-[7px_31.2px_52.6px_0] w-[61.8px] h-[54.9px]" />
+    <div class="relative m-[2px_23.1px_11.5px_0] flex flex-row justify-center p-[40px_3.9px_36px_0] w-[184.9px] h-[fit-content] box-sizing-border">
+      <img class="absolute left-[0px] top-[0px] right-[0px] bottom-[0px] h-[100%]" />
+      <span class="relative break-words font-['Short_Stack'] font-normal text-[20px] uppercase text-[#000000]">
+        Setting
+      </span>
+    </div>
+    <div class="relative m-[2px_26.1px_4.2px_0] flex flex-row justify-center p-[41px_0_42.3px_0px] w-[181px] h-[fit-content] box-sizing-border">
+      <img class="absolute left-[0px] top-[0px] right-[0px] bottom-[0px] h-[100%]" />
+      <span class="relative break-words font-['Short_Stack'] font-normal text-[20px] text-[#000000]">
+        CHARACTERS
+      </span>
+    </div>
+    <div class="relative m-[0_23.2px_13.5px_0] flex flex-row justify-center p-[42px_2px_39px_0] w-[184.9px] h-[fit-content] box-sizing-border">
+      <img class="absolute left-[0px] top-[0px] right-[0px] bottom-[0px] h-[100%]" />
+      <span class="relative break-words font-['Short_Stack'] font-normal text-[20px] leading-[1] text-[#000000]">
+        PLOT
+      </span>
+    </div>
+    <div class="relative m-[1px_27.6px_8.3px_0] flex flex-row justify-center p-[28px_0_17.1px_0.7px] w-[176px] h-[fit-content] box-sizing-border">
+      <img class="absolute left-[0px] top-[0px] right-[0px] bottom-[0px] h-[100%]" />
+      <span class="relative text-center break-words font-['Short_Stack'] font-normal text-[20px] leading-[1] text-[#000000]">
+        PROBLEM<br />
+        OR<br />
+        CONFLICT
+      </span>
+    </div>
+    <div class="relative m-[15.3px_0_0_0] flex flex-row justify-center p-[34.6px_0_39.6px_0px] w-[174.5px] h-[fit-content] box-sizing-border">
+      <img class="absolute left-[0px] top-[0px] right-[0px] bottom-[0px] h-[100%]" />
+      <span class="relative break-words font-['Short_Stack'] font-normal text-[20px] text-[#000000]">
+        RESOLUTION
+      </span>
+    </div>
+  </div>
+  <div class="relative m-[0_97px_29px_97px] inline-block self-start break-words font-['Short_Stack'] font-normal text-[24px] leading-[1.208] text-[#1E242F]">
+    You must be familiar with some of these. If not, do not worry! we will cover all these elements as we go along.
+  </div>
+  <div class="relative m-[0_19px_25.3px_0.1px] flex flex-row w-[fit-content] box-sizing-border">
+    <img class="m-[96px_18.5px_0_0] w-[66.3px] h-[60.7px]" />
+    <div class="shadow-[0px_8px_2px_0px_rgba(0,0,0,0.25)] rounded-[20px] bg-[#FE6784] relative m-[0_0_23.7px_0] flex flex-row justify-center p-[16px_69.2px_30px_47px] box-sizing-border">
+      <img class="absolute top-[2.8px] right-[9px] w-[998.2px] h-[122.2px]" />
+      <span class="relative break-words font-['Short_Stack'] font-normal text-[24px] leading-[1.208] text-[#1E242F]">
+        For today’s lesson, we will try to understand and practice writing the setting for our stories.<br />
+        <br />
+        The setting is an important element of every fiction story.<br />
+        <br />
+        
+      </span>
+    </div>
+  </div>
+  <div class="relative m-[0_0_0_65px] flex flex-row justify-center p-[3px_0_0_9px] w-[1064px] box-sizing-border">
+    <div class="relative flex flex-row gap-[0_11px] w-[179px] h-[fit-content] box-sizing-border">
+      <img class="w-[41px] h-[45px]" />
+      <div class="rounded-[8px] border-[2px_solid_#F8EBFD] bg-[#AA1070] relative m-[4px_0_4px_0] flex flex-row justify-center p-[1px_1px_2px_1px] w-[75px] h-[fit-content] box-sizing-border">
+        <div class="rounded-[8px] bg-[#CD3F97] relative flex flex-row justify-between p-[5px_8px_4px_0] w-[73px] h-[fit-content] box-sizing-border">
+          <span class="m-[0_12.5px_0_0] w-[20.3px] break-words font-['Short_Stack'] font-normal text-[20px] text-[#FFFFFF]">
+            1
+          </span>
+          <img class="m-[10px_0_10px_0] w-[10px] h-[5px]" />
+        </div>
+        <div class="rounded-[2px] bg-[#ED75C0] absolute right-[26px] bottom-[1px] w-[1px] h-[35px]">
+        </div>
+        <div class="rounded-[2px] bg-[#AA1070] absolute right-[25px] bottom-[1px] w-[1px] h-[35px]">
+        </div>
+      </div>
+      <img class="w-[41px] h-[45px]" />
+    </div>
+    <div class="absolute left-[0px] top-[0px] right-[0px] bottom-[0px] flex flex-row justify-between h-[100%] box-sizing-border">
+      <img class="w-[48px] h-[48px]" />
+      <img class="w-[48px] h-[48px]" />
+    </div>
+  </div>
+  <div class="shadow-[0px_0px_10px_0px_rgba(0,0,0,0.25)] rounded-[5px] border-[3px_dashed_#A4FDFB] bg-[#25A3A1] absolute left-[84px] top-[250px] right-[84px] p-[11px_14px_16px_14px] box-sizing-border">
+    <span class="relative break-words font-['Short_Stack'] font-normal text-[24px] leading-[1.208] text-[#FFFFFF]">
+      There are       elements which make the foundation for story writing. An element is an essential part of something and every fiction story has the same key elements:
+    </span>
+    <span class="border-[0.5px_solid_#FFFFFF] absolute left-[151px] top-[12px] break-words font-['Short_Stack'] font-normal text-[24px] leading-[1.208] text-[#FFFFFF]">
+      five
+    </span>
+  </div>
+  <img class="absolute top-[248px] right-[101.3px] w-[110.7px] h-[131.3px]" />
+</div>
+
+      <div class="bg-[#FFFFFF] flex flex-col p-[84px_0_12px_0] w-full box-sizing-border">
+        <img class="absolute top-[0px] right-[-348.9px] w-[1876.6px] h-[834px]" />
+        <img class="absolute top-[648px] right-[-9.2px] w-[82.2px] h-[77.1px]" />
+        <img class="rotate-[-65.733deg] absolute top-[97px] right-[-37.1px] w-[124.1px] h-[122.8px]" />
+        <div class="shadow-[0px_4px_4px_0px_rgba(255,255,255,0.25)] bg-[url('https://fn12.com/api/clusters/cluster-fn12-prod-europe-west1/trialprojects/trial-TQrcZ7Fh49hShjUThC7ojC/assets/images/3a8590a2cec8fa99d0474355d797e1561651c8d4?Expires=1712400223&KeyName=fn12-cdn&Signature=smpmc0y7pXFetCPD4ve8XDj96_Y=')_50%_/_cover_no-repeat] rotate-[-17.103deg] absolute left-[-25px] top-[-23px] w-[179.7px] h-[134px]"></div>
+        <div class="relative m-[0_0_19px_0] flex flex-row self-start w-[fit-content] box-sizing-border">
+          <img class="m-[37px_20.6px_42.7px_0] w-[61.4px] h-[37.3px]" />
+          <span class="relative break-words font-['Short_Stack'] font-normal text-[32px] text-[#1E242F]">
+            Can you figure out the definition of setting from the following
+            examples?
+            <br />
+            <br />
+            <br />
+          </span>
+        </div>
+        <div class="relative m-[0_0_21px_0] flex flex-row w-[fit-content] box-sizing-border">
+          <img class="m-[169px_182.2px_122.1px_0] w-[61.8px] h-[54.9px]" />
+          <div class="relative m-[0_23px_0_0] flex flex-col box-sizing-border">
+            <div class="shadow-[inset_0px_0px_10px_0px_rgba(0,0,0,0.1))] rounded-[10px] border-[3px_dashed_#CCCCCC] bg-[#FFFFFF] relative m-[0_22px_4px_22px] flex flex-row justify-center self-start p-[14px_0_14.6px_0.4px] w-[235px] h-[168px] box-sizing-border">
+              <div class="flex flex-row justify-center w-[170.4px] h-[139.4px] box-sizing-border">
+                <img class="opacity-80 w-[170.4px] h-[139.4px]" />
+              </div>
+            </div>
+            <div class="m-[0_16px_101px_16px] inline-block self-start break-words font-['Short_Stack'] font-normal text-[20px] leading-[1.45] text-[#000000]">
+              Sunny day at a beach
+            </div>
+            <div class="shadow-[inset_0px_0px_10px_0px_rgba(0,0,0,0.1))] rounded-[10px] border-[3px_dashed_#CCCCCC] bg-[#FFFFFF] w-[637px] h-[44px]"></div>
+          </div>
+          <div class="relative m-[312.2px_48.8px_4.8px_0] inline-block break-words font-['Short_Stack'] font-normal text-[20px] leading-[1.45] text-[#1E242F]">
+            Hint:
+          </div>
+          <div class="m-[120px_0_154px_0] flex flex-row justify-center w-[66.4px] h-[72px] box-sizing-border">
+            <div class="flex flex-row justify-center w-[66.4px] h-[72px] box-sizing-border">
+              <img class="w-[66.4px] h-[72px]" />
+            </div>
+          </div>
+          <div class="shadow-[inset_0px_0px_10px_0px_rgba(0,0,0,0.1))] rounded-[10px] border-[3px_dashed_#CCCCCC] bg-[#FFFFFF] absolute top-[0px] right-[289.4px] flex flex-row justify-center p-[17px_0px_18px_0] w-[235px] h-[168px] box-sizing-border">
+            <img class="w-[163px] h-[133px]" />
+          </div>
+          <span class="absolute right-[254.7px] bottom-[121px] text-center break-words font-['Short_Stack'] font-normal text-[20px] text-[#000000]">
+            A cold rainy night in a <br />
+            haunted house in October
+          </span>
+          <span class="absolute left-[82px] bottom-[8px] break-words font-['Short_Stack'] font-normal text-[20px] leading-[1.45] text-[#1E242F]">
+            So, what do you think the definition of setting is?
+            <br />
+            <br />
+            <br />
+            Any guesses?
+          </span>
+          <span class="absolute left-[248px] bottom-[10px] break-words font-['Short_Stack'] font-normal text-[24px] leading-[1.208] text-[#B6B6B6]">
+            ________________________________________
+          </span>
+        </div>
+        <div class="shadow-[0px_0px_10px_0px_rgba(0,0,0,0.25)] rounded-[5px] border-[3px_dashed_#A4FDFB] bg-[#25A3A1] relative m-[0_84px_10px_84px] flex flex-row justify-center p-[10px_85.4px_9px_14px] box-sizing-border">
+          <span class="relative break-words font-['Short_Stack'] font-normal text-[24px] leading-[1.375] text-[#FFFFFF]">
+            Setting is the time and place of a story. It often answers the
+            questions: when? and where?
+          </span>
+          <div class="absolute left-[276px] top-[9px] flex flex-row justify-between w-[217px] box-sizing-border">
+            <div class="flex flex-row justify-center w-[34.7px] h-[34.7px] box-sizing-border">
+              <img class="w-[34.7px] h-[34.7px]" />
+            </div>
+            <div class="m-[0_0_0px_0] flex flex-row justify-center w-[34.7px] h-[34.7px] box-sizing-border">
+              <img class="w-[34.7px] h-[34.7px]" />
+            </div>
+          </div>
+        </div>
+        <div class="relative m-[0_0.1px_17px_0.1px] flex flex-row self-start w-[fit-content] box-sizing-border">
+          <img class="m-[6px_15.5px_8.3px_0] w-[66.3px] h-[60.7px]" />
+          <span class="break-words font-['Short_Stack'] font-normal text-[20px] text-[#1E242F]">
+            The time of the story could be in the past, future, day, night,
+            summer or winter.
+            <br />
+            A story may take place in a school, a mall, a desert, an airplane or
+            in a variety
+            <br />
+            of other places.
+          </span>
+        </div>
+        <div class="relative m-[0_65px_0_65px] flex flex-row justify-center p-[3px_0_0_9px] w-[1064px] box-sizing-border">
+          <div class="relative flex flex-row gap-[0_11px] box-sizing-border">
+            <img class="w-[41px] h-[45px]" />
+            <div class="rounded-[8px] border-[2px_solid_#F8EBFD] bg-[#AA1070] relative m-[4px_0_4px_0] flex flex-row justify-center p-[1px_1px_2px_1px] box-sizing-border">
+              <div class="rounded-[8px] bg-[#CD3F97] relative flex flex-row p-[5px_8px_4px_19.2px] box-sizing-border">
+                <span class="m-[0_23.2px_0_0] break-words font-['Short_Stack'] font-normal text-[20px] text-[#FFFFFF]">
+                  2
+                </span>
+                <img class="m-[10px_0_10px_0] w-[10px] h-[5px]" />
+              </div>
+              <div class="rounded-[2px] bg-[#ED75C0] absolute right-[26px] bottom-[1px] w-[1px] h-[35px]"></div>
+              <div class="rounded-[2px] bg-[#AA1070] absolute right-[25px] bottom-[1px] w-[1px] h-[35px]"></div>
+            </div>
+            <img class="w-[41px] h-[45px]" />
+          </div>
+          <div class="absolute left-[0px] top-[0px] right-[0px] bottom-[0px] flex flex-row justify-between h-[100%] box-sizing-border">
+            <img class="w-[48px] h-[48px]" />
+            <img class="w-[48px] h-[48px]" />
+          </div>
+        </div>
+        <span class="absolute left-[82px] top-[176px] break-words font-['Short_Stack'] font-normal text-[24px] text-[#1E242F]">
+          (take a minute to think about this)
+        </span>
+        <div class="absolute right-[114px] bottom-[265.2px] flex flex-row box-sizing-border">
+          <div class="m-[0_14.5px_0_0] flex flex-row justify-center w-[47.8px] h-[47.8px] box-sizing-border">
+            <img class="w-[47.8px] h-[47.8px]" />
+          </div>
+          <div class="m-[0_0_0px_0] flex flex-row justify-center w-[47.7px] h-[47.7px] box-sizing-border">
+            <img class="w-[47.7px] h-[47.7px]" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/pages/test.js
@@ -0,0 +1,15 @@
+function Test() {
+  return (
+    <>
+      <div className="bg-[pink] w-[100vw] h-[100vh] flex justify-center items-center">
+        <div className="bg-white w-[90vw] h-[85vh] text-center rounded-[15px] ">
+          <div className="flex ">
+            <div className="border-2 border-dashed border-[yellow] "><p>Elements of Story Writing</p></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Test;
