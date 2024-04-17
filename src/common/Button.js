cf831a9e1b178a9fa88f7e3d50eb438692dcdae8
Mahad Ahmad <mahadahmad921@gmail.com>
2024-04-17 07:26:29 +0100

    update: code cleanup

--- src/common/Button.js
+++ src/common/Button.js
@@ -4,7 +4,7 @@ const Button = ({ right, prevSlide, nextSlide }) => {
   const dynamicClassName = `${right ? "rotate-180 " : ""} w-10 cursor-pointer`;
   return (
     <div onClick={right ? prevSlide : nextSlide}>
-      <img className={dynamicClassName} src="/button_left.svg" />
+      <img className={dynamicClassName} src="/button.svg" />
     </div>
   );
 };
